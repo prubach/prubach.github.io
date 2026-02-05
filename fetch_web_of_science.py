1550f1ea05e34aeec5ab9a88315322aeb478fc45
Paweł Rubach <pawel.rubach@gmail.com>
2026-02-05 17:42:13 +0100

    Fix publications citations

--- fetch_web_of_science.py
+++ fetch_web_of_science.py
@@ -148,6 +148,7 @@ class WebOfScience(Publications):
         self.publications = pubs
         data = fetch_data()
         self.try_match_publications(data)
+        print(self.publications)
         self.save_publications()
         if citations:
             self.update_citations_json(data, citations)
